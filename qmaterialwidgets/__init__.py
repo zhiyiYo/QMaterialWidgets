3797c65a2903d16c99306171983231cb4802b4fe
zhiyiYo <1319158137@qq.com>
2023-08-30 17:39:03 +0800

    更新版本号

--- qmaterialwidgets/__init__.py
+++ qmaterialwidgets/__init__.py
@@ -12,7 +12,7 @@ Examples are available at https://github.com/zhiyiYo/QMaterialWidgets/tree/PyQt6
 :license: PyQt6-Material-Widgets is licensed under [GPLv3](./LICENSE).
 """
 
-__version__ = "0.1.3"
+__version__ = "0.9.9"
 
 from .components import *
 from .common import *
